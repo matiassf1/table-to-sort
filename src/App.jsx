ae1f38655ac7d781e5484599872db657f1ef2cd3
Matias Sfer <matias0developer@gmail.com>
2023-07-03 16:54:29 -0300

    setting app // table.jsx added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Table } from "./components/Table"
+
+export const App = () => {
+  return (
+    <div className="content">
+      <h2>Hello - App!</h2>
+      <span>26</span>
+      <Table />
+    </div>
+  )
+}
